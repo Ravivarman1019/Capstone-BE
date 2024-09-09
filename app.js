0febd1e616d75b19858145a687985a8236e80b83
Ravivarman1019 <ravivarman6242@gmail.com>
2024-09-09 10:52:18 +0530

    commit

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-import express from 'express';
-import connectDB from './config/dp.js';
-import userRoutes from './routes/auth.js';
-import emailRoutes from './routes/email.js';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import db from './config/dp.js';
-
-
+import app from './app.js';
+import connectDB from './config/dp.js';
+import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-connectDB();
-
-
-app.use(express.json());
-
-
-app.use('/api/users',userRoutes);
-app.use('/api/emails', emailRoutes);
-
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send({ message: 'Something went wrong' });
-});
-export default app;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true, 
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
