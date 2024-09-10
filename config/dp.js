efa297b3082c1fe030fdb616dd5b1538ccccfcad
Ravivarman1019 <ravivarman6242@gmail.com>
2024-09-10 10:51:18 +0530

    commit

--- config/dp.js
+++ config/dp.js
@@ -1,16 +1,9 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config();  
+const mongoURI = process.env.MONGODB_URI;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); 
-  }
-};
-
-export default connectDB;
+mongoose.connect(mongoURI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('Error connecting to MongoDB:', err));
